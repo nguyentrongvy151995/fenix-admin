c8371122fcef038f9ff9da76de686e40e15d8533
vynt-gcode <vynt.gcode@gmail.com>
2023-09-17 18:11:56 +0700

    fix conflict

--- bin/routes.js
+++ bin/routes.js
@@ -3,12 +3,9 @@ const foodAPI = require('../modules/food/express-api');
 const authAPI = require('../modules/auth/express-api');
 const userInfoAPI = require('../modules/user_info/express-api');
 const rankTiersAPI = require('../modules/rank-tiers/express-api');
-<<<<<<< Updated upstream
-=======
 const rankSettingAPI = require('../modules/rank_setting/express-api');
 const matchMakingAPI = require('../modules/match-making/express-api');
 const matchSetting = require('../modules/match-setting/express-api');
->>>>>>> Stashed changes
 
 module.exports = app => {
     // import api
@@ -16,10 +13,7 @@ module.exports = app => {
     authAPI.load(app);
     userInfoAPI.load(app);
     rankTiersAPI.load(app);
-<<<<<<< Updated upstream
-=======
     rankSettingAPI.load(app);
     matchMakingAPI.load(app);
     matchSetting.load(app);
->>>>>>> Stashed changes
 };
